53ff1216d6767b5263965a5195f1b3fdd0777787
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 00:54:48 +0100

    Couple notes removed

--- src/main/java/net/patrick/bestmod/BestMod.java
+++ src/main/java/net/patrick/bestmod/BestMod.java
@@ -18,10 +18,8 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BestMod.MOD_ID)
 public class BestMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "best_mod";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
     public BestMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
